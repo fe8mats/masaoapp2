6506ce276c4b0968fd79bb5203f0ec79250365fc
393mats <tex419@gmail.com>
2020-05-10 16:13:49 +0900

    deleted uikit

--- src/dataManager.js
+++ src/dataManager.js
@@ -1,12 +1,32 @@
+/* eslint-disable no-console */
 export default class DataManager {
-  constructor() {
-    this.mapData = '............................................................';
+  constructor(layermode = 0, x = 180, y = 30) {
+    this.mapData = '';
+    this.layData = '';
+    this.layerMode = layermode;
+    this.layerX = 180;
+    this.layerY = 30;
+    if (this.layerMode !== 0) {
+      this.layerX = x;
+      this.layerY = y;
+    }
   }
 
   // ---------------------------------------------------------
   // Mapdata
   // ---------------------------------------------------------
   createMapdata() {
-    // create
+    this.mapData = '............................................................';
+    this.layData = this.mapData + this.mapData;
+
+    switch (this.layerMode) {
+      case 1:
+
+        break;
+
+      default:
+        console.log('normal-layer');
+        break;
+    }
   }
 }
